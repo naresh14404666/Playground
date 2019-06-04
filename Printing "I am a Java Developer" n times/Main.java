4a1c38d4d5b127b94de71614f3efc1073eaabbf5
D.Naresh <45494828+naresh14404666@users.noreply.github.com>
2019-06-04 16:38:20 +0530

    Test code pushed to repository.

---
+++ Printing "I am a Java Developer" n times/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main {
+	public static void main (String[] args){
+      int numTimes=7;
+  String myStr = "I am a Java Developer";
+		
+   for(int counter=0; counter < numTimes; counter++)
+   {
+	System.out.println(myStr);
+	    // Type your code here
+   }
+    }
+}
